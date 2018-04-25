ae70ef916b13673ceb08afdfd40b7830edadafae
Kris Penney <kris9101@gmail.com>
2018-04-25 19:46:24 -0400

    reindent everything

--- Distortion/Source/PluginEditor.cpp
+++ Distortion/Source/PluginEditor.cpp
@@ -1,23 +1,42 @@
 /*
-  ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ This file was auto-generated!
+ 
+ It contains the basic framework code for a JUCE plugin editor.
+ 
+ ==============================================================================
+ */
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
 //==============================================================================
 DistortionAudioProcessorEditor::DistortionAudioProcessorEditor (DistortionAudioProcessor& p)
-    : AudioProcessorEditor (&p), processor (p)
+: AudioProcessorEditor (&p), processor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+  addAndMakeVisible(driveSlider = new Slider("Drive"));
+  driveSlider->setSliderStyle(Slider::Rotary);
+  driveSlider->setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+  
+  addAndMakeVisible(rangeSlider = new Slider("Range"));
+  rangeSlider->setSliderStyle(Slider::Rotary);
+  rangeSlider->setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+  
+  addAndMakeVisible(blendSlider = new Slider("Blend"));
+  blendSlider->setSliderStyle(Slider::Rotary);
+  blendSlider->setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+  
+  addAndMakeVisible(volumeSlider = new Slider("Volume"));
+  volumeSlider->setSliderStyle(Slider::Rotary);
+  volumeSlider->setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+  
+  driveAttachment = new AudioProcessorValueTreeState::SliderAttachment();
+  
+  
+  // Make sure that before the constructor has finished, you've set the
+  // editor's size to whatever you need it to be.
+  setSize (500, 200);
 }
 
 DistortionAudioProcessorEditor::~DistortionAudioProcessorEditor()
@@ -27,16 +46,16 @@ DistortionAudioProcessorEditor::~DistortionAudioProcessorEditor()
 //==============================================================================
 void DistortionAudioProcessorEditor::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    g.setColour (Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+  // (Our component is opaque, so we must completely fill the background with a solid colour)
+  g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+  
+  g.setColour (Colours::white);
+  g.setFont (15.0f);
+  g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
 }
 
 void DistortionAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+  // This is generally where you'll want to lay out the positions of any
+  // subcomponents in your editor..
 }
--- Distortion/Source/PluginEditor.h
+++ Distortion/Source/PluginEditor.h
@@ -1,12 +1,12 @@
 /*
-  ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ This file was auto-generated!
+ 
+ It contains the basic framework code for a JUCE plugin editor.
+ 
+ ==============================================================================
+ */
 
 #pragma once
 
@@ -15,21 +15,31 @@
 
 //==============================================================================
 /**
-*/
+ */
 class DistortionAudioProcessorEditor  : public AudioProcessorEditor
 {
 public:
-    DistortionAudioProcessorEditor (DistortionAudioProcessor&);
-    ~DistortionAudioProcessorEditor();
-
-    //==============================================================================
-    void paint (Graphics&) override;
-    void resized() override;
-
+  DistortionAudioProcessorEditor (DistortionAudioProcessor&);
+  ~DistortionAudioProcessorEditor();
+  
+  //==============================================================================
+  void paint (Graphics&) override;
+  void resized() override;
+  
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    DistortionAudioProcessor& processor;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DistortionAudioProcessorEditor)
+  // This reference is provided as a quick way for your editor to
+  // access the processor object that created it.
+  DistortionAudioProcessor& processor;
+  
+  ScopedPointer<Slider> driveSlider;
+  ScopedPointer<Slider> rangeSlider;
+  ScopedPointer<Slider> blendSlider;
+  ScopedPointer<Slider> volumeSlider;
+  
+  ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> driveAttachment;
+  ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> rangeAttachment;
+  ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> blendAttachment;
+  ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> volumeAttachment;
+  
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DistortionAudioProcessorEditor)
 };
